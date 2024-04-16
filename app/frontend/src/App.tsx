1437c52fbc5d03b339fa304376213dd889c5bcc2
Mateus Tapias <mateustapias2015@gmail.com>
2024-04-16 16:25:22 -0300

    Rotas criadas. Fundo. fonte e cor da aplicação criados.

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,11 +1,71 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css'
 
 function App() {
   return (
-    <h1>
-      oi
-    </h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
 export default App;
+
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <h1>
+//       oi
+//     </h1>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// const App: React.FC = () => {
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   const [stars, setStars] = useState<any[]>([]);
+//   const name = 'proxima';
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('https://api.api-ninjas.com/v1/stars', {
+//           params: { name },
+//           headers: {
+//             'X-Api-Key': '9JJTx6tErKRRT9Uagjl8cw==ZiNng0p70ANHmnJw'
+//           }
+//         });
+//         setStars(response.data);
+//       } catch (error) {
+//         console.error('Request failed:', error);
+//       }
+//     };
+
+//     fetchData();
+//   }, [name]);
+
+//   return (
+//     <div>
+//       <h1>Star Data:</h1>
+//       <ul>
+//         {stars.map((star, index) => (
+//           <li key={index}>{JSON.stringify(star)}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
---
+++ app/frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <h1>
+      oi
+    </h1>
+  );
+}
+
+export default Home;
