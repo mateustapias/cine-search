7c8074d689fc4556b363d24445910aaf2daf9089
Mateus Tapias <mateustapias2015@gmail.com>
2024-05-02 18:11:47 -0300

    Problema TS2307 consertado no Header

--- app/frontend/src/components/Header.tsx
+++ app/frontend/src/components/Header.tsx
@@ -1,12 +1,12 @@
 import '../styles/components/Header.css';
+import popcornBox from '../assets/images/popcornBox.png';
 
 const Header = () => {
   return (
-    <>
-      <div className='headerContainer'>
-        <p>oi</p>
-      </ div>
-    </>
+    <div className='headerContainer'>
+      <img src={popcornBox} alt="Logo" className="headerImage" />
+      <p>oi</p>
+    </div>
   );
 };
 
