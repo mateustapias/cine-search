5e7b65d2c2d410600dff61947fd60591ddef6bfb
Mateus Tapias <mateustapias2015@gmail.com>
2024-05-08 14:58:15 -0300

    Estilização de login e cadastro aprimorados

--- app/frontend/src/components/LogInOrSignUpPrompt/LogInPrompt.tsx
+++ app/frontend/src/components/LogInOrSignUpPrompt/LogInPrompt.tsx
@@ -1,30 +1,35 @@
 import '../../styles/components/LogInOrSignUp/LogInPrompt.css';
 
 const LogInPrompt = () => {
-
   return (
-    <form className='log-in-prompt'
+    <div className='c-log-in-prompt'
     >
       <div className='c-log-in-message'>Bem-vindo de volta!</div>
-      <div className='c-email'>
-        <input
-          autoFocus
-          type='email'
-          placeholder='email'
-          className='email-input'
-        />
-      </div>
-      <div className='c-password'>
-        <input
-          type='password'
-          placeholder='senha'
-          className='password-input'
-        />
-      </div>
-      <div className='c-submit-btn'>
-        <button type='submit'>Entrar</button>
-      </div>
-    </form>
+      <form className='log-in-form'>
+        <div className='c-email'>
+          <label htmlFor='email' className='email-label'>Email</label>
+          <input
+            autoFocus
+            type='email'
+            id='email'
+            // placeholder='email'
+            className='email-input'
+          />
+        </div>
+        <div className='c-password'>
+          <label htmlFor='password' className='password-label'>Senha</label>
+          <input
+            type='password'
+            id='password'
+            // placeholder='senha'
+            className='password-input'
+          />
+        </div>
+        <div className='c-submit-btn'>
+          <button type='submit'>Entrar</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- app/frontend/src/components/LogInOrSignUpPrompt/SignUpPrompt.tsx
+++ app/frontend/src/components/LogInOrSignUpPrompt/SignUpPrompt.tsx
@@ -1,24 +1,41 @@
+import '../../styles/components/LogInOrSignUp/SignUpPrompt.css';
+
 const SignUpPrompt = () => {
   return (
-    <div className='signUpPrompt'>
-      <div className='signUpMessage'>Crie uma conta para desfrutar 100% do site</div>
-      <input
-        autoFocus
-        type='email'
-        placeholder='Email'
-        className='emailInput'
-      />
-      <input
-        type='text'
-        placeholder='Nome de usuário'
-        className='emailInput'
-      />
-      <input
-        type='password'
-        placeholder='senha'
-        className='passwordInput'
-      />
-      <button type='submit'>Entrar</button>
+    <div className='c-sign-up-prompt'>
+      <div className='c-sign-up-message'>Crie uma conta para desfrutar 100% do site</div>
+      <form className='sign-up-form'>
+        <div className='c-email'>
+          <label htmlFor='email' className='email-label'>Email</label>
+          <input
+            autoFocus
+            type='email'
+            id='email'
+            // placeholder='email'
+            className='email-input'
+          />
+        </div>
+        <div className='c-username'>
+          <label htmlFor='username' className='username-label'>Usuário</label>
+          <input
+            type='text'
+            // placeholder='Nome de usuário'
+            className='emailInput'
+          />
+        </div>
+        <div className='c-password'>
+          <label htmlFor='password' className='password-label'>Senha</label>
+          <input
+            type='password'
+            id='password'
+            // placeholder='senha'
+            className='password-input'
+          />
+        </div>
+        <div className='c-submit-btn'>
+          <button type='submit'>Entrar</button>
+        </div>
+      </form>
     </div>
   );
 };
