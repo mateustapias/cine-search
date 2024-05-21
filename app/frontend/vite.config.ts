51330e3ebdc8b9d53953b66cb008f9ffaf7c983e
Mateus Tapias <mateustapias2015@gmail.com>
2024-05-21 11:16:21 -0300

    Tentando upar o app

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,7 +1,15 @@
-export default {
-  server: {
-    port: 3000,
-    host: true,
-    hmr: true,
-  },
-};
+// export default {
+//   server: {
+//     port: 3000,
+//     host: true,
+//     hmr: true,
+//   },
+// };
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
