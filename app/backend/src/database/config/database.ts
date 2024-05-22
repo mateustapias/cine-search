cc9173b05890c1aa4c950d1682da85b62ea0c96e
Mateus Tapias <mateustapias2015@gmail.com>
2024-05-22 14:23:23 -0300

    tentativa de rodar aplicação completa

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '123456',
-  database: 'CINE_SEARCH',
+  database: 'cine-search',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
+    // port: 3000,
     host: true,
     hmr: true,
   },
