4c0002873d98e2fe53a004560eb8bb1ce988d471
Mateus Tapias <mateustapias2015@gmail.com>
2024-05-23 14:55:46 -0300

    .

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,8 +1,59 @@
+// import * as express from 'express';
+// import 'express-async-errors';
+
+// import errorMiddleware from './middlewares/errorMiddleware';
+// import router from './routes';
+
+// class App {
+//   public app: express.Express;
+
+//   constructor() {
+//     this.app = express();
+
+//     this.config();
+
+//     // Não remover essa rota
+//     this.app.get('/', (req, res) => res.json({ ok: true }));
+
+//     this.routes();
+
+//     // Não remova esse middleware de erro, mas fique a vontade para customizá-lo
+//     // Mantenha ele sempre como o último middleware a ser chamado
+//     this.app.use(errorMiddleware);
+//   }
+
+//   private config(): void {
+//     const accessControl: express.RequestHandler = (_req, res, next) => {
+//       res.header('Access-Control-Allow-Origin', '*');
+//       res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+//       res.header('Access-Control-Allow-Headers', '*');
+//       next();
+//     };
+
+//     this.app.use(express.json());
+//     this.app.use(accessControl);
+//   }
+
+//   private routes(): void {
+//     this.app.use(router);
+//   }
+
+//   public start(PORT: string | number): void {
+//     this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+//   }
+// }
+
+// export { App };
+
+// // Essa segunda exportação é estratégica, e a execução dos testes de cobertura depende dela
+// export const { app } = new App();
+
 import * as express from 'express';
 import 'express-async-errors';
 
 import errorMiddleware from './middlewares/errorMiddleware';
 import router from './routes';
+import sequelize from './database/models/index'; // Importe o Sequelize aqui
 
 class App {
   public app: express.Express;
@@ -36,6 +87,17 @@ class App {
 
   private routes(): void {
     this.app.use(router);
+
+    // Adicione a rota de teste de conexão aqui
+    this.app.get('/test-connection', async (req, res) => {
+      try {
+        await sequelize.authenticate();
+        res.send('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        res.status(500).send('Unable to connect to the database.');
+      }
+    });
   }
 
   public start(PORT: string | number): void {
