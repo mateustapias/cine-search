8dd7b6838262f6bb124eed31226f38948526e7ad
Mateus Tapias <mateustapias2015@gmail.com>
2024-05-24 14:39:31 -0300

    testando

--- app/frontend/src/services/requests.ts
+++ app/frontend/src/services/requests.ts
@@ -8,16 +8,6 @@ const api = axios.create({
   // baseURL: import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:3001',
 });
 
-// var axios = require("axios").default;
-
-// var options = { method: 'GET', url: 'https://cine-search-pxki.onrender.com/movies/many' };
-
-// axios.request(options).then(function (response) {
-//   console.log(response.data);
-// }).catch(function (error) {
-//   console.error(error);
-// });
-
 export const setToken = (token: Token) => {
   api.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
@@ -37,21 +27,10 @@ export const requestSignUp = async (endpoint: string, body: SignUp) => {
   return data;
 };
 
-// export const requestMovies = async (endpoint: string) => {
-//   const { data } = await api.get(endpoint);
-//   return data;
-// };
-
 export const requestMovies = async (endpoint: string) => {
-  const options = { method: 'GET', url: 'https://cine-search-pxki.onrender.com/movies/many' };
-
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-    return response
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
+  const { data } = await api.get(endpoint);
+  return data;
+};
 
 
 export default api;
