f28e0afdb0c9fb74cbaf65df5375f07436094421
Mateus Tapias <mateustapias2015@gmail.com>
2024-06-21 16:12:38 -0300

    Ajustado tamanho do containers de detalhes do filme para serem relativos

--- app/frontend/src/pages/MovieDetails.tsx
+++ app/frontend/src/pages/MovieDetails.tsx
@@ -28,28 +28,16 @@ const MovieDetails = () => {
     <>
       <div className='c-movie-details-a'>
         {
-          (movieData && (
-            <>
-              <MovieMainDetails movieData={movieData} />
-              <MovieSecondaryDetails movieData={movieData} />
-            </>
-          )) || (
-            <div style={{ flex: 1 }}>
-              <Skeleton
-                width={'100%'}
-                height={'100%'}
-              />
-            </div>
-          )
-        }
-        {/* {movieData && (
-          <>
+          (movieData && (<>
             <MovieMainDetails movieData={movieData} />
             <MovieSecondaryDetails movieData={movieData} />
-          </>
-        )} */}
+          </>)
+          ) || (
+            <div style={{ flex: 1 }}>
+              <Skeleton width={'100%'} height={'100%'} />
+            </div>
+          )}
       </div >
-      {/* Não sei se é melhor passar o id como prop ou usar useParams no componente */}
       <MovieReviews id={Number(id)} />
     </>
   );
